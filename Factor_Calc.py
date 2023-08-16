ae6150af1ad8654451955fa5a8b9aa59834e42d5
SwathedSplash62 <138179868+SwathedSplash62@users.noreply.github.com>
2023-08-17 09:56:33 +1200

    Add files via upload

--- Factor_Calc.py
+++ Factor_Calc.py
@@ -12,4 +12,73 @@ def statement_generator(text, decoration):
     return ""
 
 
-statement_generator("Calculator for Factors of Numbers", "*")
+def num_check(question):
+    valid = False
+    while not valid:
+        error = "Please enter something that is more than zero"
+
+
+        try:
+
+            #ask user to enter a number
+            response = float(input(question))
+
+            #checks number is more than zero
+            if response > 0:
+                return response
+                print(error)
+                print()
+
+
+            #Outputs error if input is invalid
+            else:
+                print(error)
+                print()
+
+        except ValueError:
+                 print(error)
+                 print()
+
+
+keep_going = ""
+while keep_going == "":
+
+    comment = ""
+
+    # ask user for a number to be factored
+    var_to_factor = num_check("Number? ")
+
+    if var_to_factor != 1:
+        factor_list = (var_to_factor)
+    else:
+        factor_list = ""
+        comment = "One is Unity, it only has one factor that being itself"
+
+    # comments for squares / primes
+    if len(factor_list) == 2:
+        comment = "{} is a prime number.".format(var_to_factor)
+    elif len(factor_list) % 2 == 1:
+        comment = "{} is a perfect square".format(var_to_factor)
+
+    # output factors and comment
+
+    # Generate heading
+    if var_to_factor == 1:
+        heading = "One is special. . ."
+
+    else:
+        heading = "Factors of {}".format(var_to_factor)
+
+    # Output factors and comment
+    statement_generator(heading, "*")
+    print()
+    print(factor_list)
+    print(comment)
+
+    print()
+    keep_going = input("Press <enter> to continue or any key to quit ")
+    print()
+
+print()
+print("Thank you for using the factors calculator")
+print()
