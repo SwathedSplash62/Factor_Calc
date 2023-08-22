6e725e251dce65a0229e2a738dba039e624c162b
SwathedSplash62 <138179868+SwathedSplash62@users.noreply.github.com>
2023-08-23 11:04:56 +1200

    Add files via upload

--- ZZ_list_play.py
+++ ZZ_list_play.py
@@ -20,3 +20,12 @@ print()
 print(my_list)
 print("The list has {} items".format(my_list_len))
 print()
+
+
+def factors(my_list, the_number, the_factor=3):
+    if the_number % the_factor == 0:
+        print(the_factor)
+        my_list.append(the_factor)
+        factors(the_number, the_factor)
+    elif the_factor <= the_number:
+        factors(the_number, the_factor+2)
